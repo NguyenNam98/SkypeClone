2045b2d71f36edef503397eaa6c41e40f78b9380
michael98 <namnv78@fpt.com.vn>
2021-08-18 22:34:23 +0700

    update list group

--- client/src/context/user.context.js
+++ client/src/context/user.context.js
@@ -1,18 +1,32 @@
 import React, {useState} from 'react';
 
 export const UserContext= React.createContext();
+const host = process.env.REACT_APP_HOST
+const port = process.env.REACT_APP_PORT || 8080
 
 export function UserProvider (props){
     const [userInfo,setUserInfo]= useState({})
+    const [firstPage, setFisrtPage] = useState(true)
+    const [listGroups , setListGroups] = useState([])
     const setUserInfoFunc =(user)=>{
         setUserInfo(Object.assign(user, userInfo))
     }
-    
+    const setToFisrtPage =()=>{
+        setFisrtPage(false)
+    }
+    const setToListGroups =(groups)=>{
+        setListGroups(groups)    
+    }
+
     return(
         <UserContext.Provider
             value={{
                 userInfo:userInfo,
-                setUserInfo:setUserInfoFunc
+                setUserInfo:setUserInfoFunc,
+                firstPage: firstPage,
+                setToFisrtPage: setToFisrtPage,
+                listGroups: listGroups,
+                setToListGroups: setToListGroups
             }}
         >
             {props.children}
--- server/routes/groups.route.js
+++ server/routes/groups.route.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const groupController = require('../controllers/groups.controller')
 
-
 router.post('/createGroup/:idUser',groupController.createGroup)
 router.post('/addMember/', groupController.addMember)
+router.get('/listGroups', groupController.getGroup)
 
 module.exports = router
